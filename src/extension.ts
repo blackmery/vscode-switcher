07edee489fffaf9eeecaa7f4a030b61949625dae
blackmery <blackmery.github@rainystudio.com>
2016-06-10 12:49:09 +0900

    Clean code

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,51 @@ import * as fs from 'fs';
 
 namespace switcher {
 
+    //! for scode.workspace.findFiles()
+    const kFindFilesLimitCount : number = 8;
+
+    //==========================================================================
+    class SplitedPath {
+        dpath: string;
+        fname: string;
+        ename: string;
+        constructor(dpath?: string, fname?: string, ename?: string) {
+            this.dpath = dpath;
+            this.fname = fname;
+            this.ename = ename;
+        }
+    }
+
+    //==========================================================================
+    class Context {
+        splited_path    : SplitedPath;
+        first_ext_index : number;
+        extensions      : string [];
+
+        constructor() {
+            this.splited_path    = new SplitedPath();
+            this.first_ext_index = undefined;
+            this.extensions      = [];
+
+            const configuration = vscode.workspace.getConfiguration();
+            const extensions = configuration.get("switcher.findExtensionOrder");
+            for (let i in extensions) {
+                this.extensions.push(extensions[i]);
+            }
+        }
+    }
+    let context = new Context();
+
+    //==========================================================================
+    function isFindAllFilesInRootDirectory() : boolean
+    {
+        const configuration = vscode.workspace.getConfiguration();
+        if (configuration.get("switcher.findAllFilesInRootDirectory") == false) {
+            return false;
+        }
+        return true;
+    }
+
     //==========================================================================
     function getFileExtension(fname: string) : string
     {
@@ -18,7 +63,7 @@ namespace switcher {
     }
 
     //==========================================================================
-    function splitFilePath(fullpath: string)
+    function splitFilePath(fullpath: string) : SplitedPath
     {
         let dpath = fullpath;
         let fname = undefined;
@@ -36,17 +81,15 @@ namespace switcher {
                 }
             }
         }
-        return { dpath: dpath, fname: fname, ename: ename };
+        return new SplitedPath(dpath, fname, ename);
     }
 
     //==========================================================================
     function getExtensionIndex(ename: string) : number
     {
         if (ename) {
-            const configuration = vscode.workspace.getConfiguration();
-            const extensions = configuration.get("switcher.findExtensionOrder");
-            for (let i in extensions) {
-                if (extensions[i] == ename) {
+            for (let i in context.extensions) {
+                if (context.extensions[i] == ename) {
                     return Number(i);
                 }
             }
@@ -57,28 +100,125 @@ namespace switcher {
     //==========================================================================
     function getExtensionName(ext_index: number) : string
     {
-        const configuration = vscode.workspace.getConfiguration();
-        const extensions = configuration.get("switcher.findExtensionOrder");
-        const extensions_length = extensions["length"];
         let result = undefined;
-        if (ext_index >= 0 && ext_index < extensions_length) {
-            result = extensions[ext_index];
+        if (ext_index >= 0 && ext_index < context.extensions.length) {
+            result = context.extensions[ext_index];
         }
         return result;
     }
 
     //==========================================================================
     function getNextExtensionIndex(ext_index: number) : number
-    {
-        const configuration = vscode.workspace.getConfiguration();
-        const extensions = configuration.get("switcher.findExtensionOrder");
-        const extensions_length = extensions["length"]; 
+    { 
         let next_ext_index = ext_index + 1;
-        if (next_ext_index >= extensions_length) {
+        if (next_ext_index >= context.extensions.length) {
             next_ext_index = 0;
         }
         return next_ext_index;
     }
+
+    //==========================================================================
+    function openTextDocument(fpath: string) : PromiseLike<any>
+    {
+        return vscode.workspace.openTextDocument(fpath).then(
+            document => {
+                return Promise.resolve(document);
+            },
+            reason => {
+                return Promise.reject(undefined);
+            }
+        );
+    };
+
+    //==========================================================================
+    function alwaysReject() : PromiseLike<any>
+    {
+        // TODO: Microsoft/vscode: vscode debugger stops on reject() #1746  
+        // return new Promise((resolve, reject) => {
+        //     return reject(undefined);
+        // });
+        return openTextDocument("");
+    }
+
+    //==========================================================================
+    function findInRootDirectory(ext_index: number) : PromiseLike<any>
+    {
+        if (!isFindAllFilesInRootDirectory()) {
+            return alwaysReject();
+        }
+
+        let target_fpath;
+        target_fpath  = "**/";
+        target_fpath += context.splited_path.fname;
+        target_fpath += ".";
+        target_fpath += getExtensionName(ext_index);
+        const promise = vscode.workspace.findFiles(target_fpath, "", kFindFilesLimitCount).then(
+            files => {
+                if (files.length == 1) {
+                    const file = files.pop();
+                    return openTextDocument(file.fsPath);
+                } else if (files.length > 1) {
+                    let file_list = [];
+                    files.forEach(file => {
+                        file_list.push(file.fsPath);
+                    });
+
+                    return vscode.window.showQuickPick(file_list).then(
+                        file => {
+                            return openTextDocument(file);
+                        },
+                        reason => {
+                            return Promise.reject(undefined);
+                        }
+                    );
+                }
+                // files.length == 0
+                return alwaysReject();
+            },
+            reason => {
+                return Promise.reject(undefined);
+            }
+        );
+        return promise;
+    };
+
+    //==========================================================================
+    function findInSameDirectory(ext_index: number) : PromiseLike<any>
+    {
+        let target_fpath;
+        target_fpath  = context.splited_path.dpath;
+        target_fpath += "/";
+        target_fpath += context.splited_path.fname;
+        target_fpath += ".";
+        target_fpath += getExtensionName(ext_index);
+        const promise = openTextDocument(target_fpath).then(
+            document => {
+                return Promise.resolve(document);
+            },
+            reason =>  {
+                return findInRootDirectory(ext_index);
+            }
+        );
+        return promise;
+    };
+
+    //==========================================================================
+    function selectDocument(ext_index: number) : PromiseLike<any>
+    {
+        return findInSameDirectory(ext_index).then(
+            document => {
+                return Promise.resolve(document);
+            },
+            reason => {
+                const next_ext_index = getNextExtensionIndex(ext_index);
+                if (next_ext_index != context.first_ext_index) {
+                    return selectDocument(next_ext_index);
+                } else {
+                    return Promise.reject(undefined);
+                }
+            } 
+        )
+    };
 
     //==========================================================================
     export function run()
@@ -89,130 +229,31 @@ namespace switcher {
             active_document = vscode.window.activeTextEditor.document;
         }
         if (active_document == undefined) {
-            // do nothing
             return;
         }
-
+        
         // get the parts of directory path, filename and extension
-        //  - splited_path.dpath : directory path
-        //  - splited_path.fname : filename without the extension
-        //  - splited_path.ename : extension
-        const splited_path = splitFilePath(
+        context.splited_path = splitFilePath(
             // convert back-slash to slash
             active_document.fileName.replace(/\\/g, "/")
-        );
+        ); 
 
-        // 無効な拡張子なら無視
-        const first_ext_index = getExtensionIndex(splited_path.ename);
-        if (first_ext_index == undefined) {
-            console.log("Switcher: Unknown extension.");
+        // get the extension index of the active document
+        context.first_ext_index = getExtensionIndex(context.splited_path.ename);
+        if (context.first_ext_index == undefined) {
+            console.log("Switcher: unknown extension");
             return;
         }
 
-        const openTextDocument = function (fpath: string) { 
-            return vscode.workspace.openTextDocument(fpath).then(
-                document => {
-                    return Promise.resolve(document);
-                },
-                reason => {
-                    return Promise.reject(undefined);
-                }
-            );
-        };
-
-        const alwaysReject = function () {
-            return openTextDocument("");
-        };
-
-        const findInRootDirectory = function (ext_index: number) {
-            // ルートディレクトリ検索オプションが無効なら何もしない
-            const configuration = vscode.workspace.getConfiguration();
-            if (configuration.get("switcher.findAllFilesInRootDirectory") == false) {
-                return alwaysReject();
-            }
-
-            let target_fpath;
-            target_fpath  = "**/";
-            target_fpath += splited_path.fname;
-            target_fpath += ".";
-            target_fpath += getExtensionName(ext_index);
-            const promise = vscode.workspace.findFiles(target_fpath, "", 8).then(
-                files => {
-                    if (files.length == 1) {
-                        const file = files.pop();
-                        return openTextDocument(file.fsPath);
-                    } else if (files.length > 1) {
-                        let file_list = [];
-                        files.forEach(file => {
-                            file_list.push(file.fsPath);
-                        });
-
-                        return vscode.window.showQuickPick(file_list).then(
-                            file => {
-                                return openTextDocument(file);
-                            },
-                            reason => {
-                                return Promise.reject(undefined);
-                            }
-                        );
-                    }
-
-                    // files.length == 0
-                    return alwaysReject();
-                },
-                reason => {
-                    return Promise.reject(undefined);
-                }
-            );
-            return promise;
-        };
-
-        const findInSameDirectory = function (ext_index: number) {
-            // 同一ディレクトリを検索
-            let target_fpath;
-            target_fpath  = splited_path.dpath;
-            target_fpath += "/";
-            target_fpath += splited_path.fname;
-            target_fpath += ".";
-            target_fpath += getExtensionName(ext_index);
-            const promise = openTextDocument(target_fpath).then(
-                document => {
-                    return Promise.resolve(document);
-                },
-                reason =>  {
-                    // ルートディレクトリ以下のファイルを検索
-                    return findInRootDirectory(ext_index);
-                }
-            );
-            return promise;
-        };
-
-        const selectDocument = function (ext_index: number) {
-            return findInSameDirectory(ext_index).then(
-                document => {
-                    // 選択されたドキュメントを返す
-                    return Promise.resolve(document);
-                },
-                reason => {
-                    const next_ext_index = getNextExtensionIndex(ext_index);
-                    if (next_ext_index != first_ext_index) {
-                        return selectDocument(next_ext_index);
-                    } else {
-                        return Promise.reject(undefined);
-                    }
-                } 
-            )
-        };
-
         // ドキュメントの選別を開始
-        const next_ext_index = getNextExtensionIndex(first_ext_index);
+        const next_ext_index = getNextExtensionIndex(context.first_ext_index);
         selectDocument(next_ext_index).then(
             document => {
-                console.log("done: " + document.fileName);
+                console.log("Switcher: " + document.fileName);
                 vscode.window.showTextDocument(document);
             },
             reason => {
-                console.log("done: file not found.");
+                console.log("Switcher: file not found");
             }
         );
     }
